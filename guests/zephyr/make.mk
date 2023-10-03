880060da3e512ca498d7b270237faa9cf64e6030
Yan-Jie Wang <yanjiewtw@gmail.com>
2023-10-03 16:50:30 +0800

    update(guests/zephyr): update zephyr to version 3.5.0-rc1
    Update zephyr to version 3.5.0-rc1.
    
    In addition to version update for zephyr, also specify the commit hash
    for cmsis.
    
    Signed-off-by: Yan-Jie Wang <yanjiewtw@gmail.com>

--- guests/zephyr/make.mk
+++ guests/zephyr/make.mk
@@ -1,8 +1,9 @@
 zephyr_src:=$(wrkdir_src)/zephyr
 zephyr_repo:=https://github.com/zephyrproject-rtos/zephyr.git
-zephyr_version:=v3.3.0
+zephyr_version:=v3.5.0-rc1
 zephyr_cmsis_repo:=https://github.com/zephyrproject-rtos/cmsis.git
 zephyr_cmsis_src:=$(wrkdir_src)/cmsis
+zephyr_cmsis_commit:=5a00331455dd74e31e80efa383a489faea0590e3
 zephyr_build:=$(wrkdir_demo_imgs)/zephyr_build
 zephyr_board_root:=$(bao_demos)/guests/zephyr
 zephyr_board:=baovm_$(PLATFORM)
@@ -20,6 +21,7 @@ zephyr_bin:=$(zephyr_build)/zephyr/zephyr.bin
 
 $(zephyr_cmsis_src):
 	git clone $(zephyr_cmsis_repo) $@
+	git -C $(zephyr_cmsis_src) checkout $(zephyr_cmsis_commit)
 
 $(zephyr_build): $(zephyr_src) $(zephyr_cmsis_src)
 	$(zephyr_env) cmake -DCMAKE_PREFIX_PATH=$(zephyr_src)/share/zephyr-package \
