b217fb456ae6f57e3d2c771e17c030dbffe81774
Jose Martins <josemartins90@gmail.com>
2025-07-19 12:47:09 +0100

    update: bump qemu's riscv hart spec version and add sstc extension
    Signed-off-by: Jose Martins <josemartins90@gmail.com>

--- platforms/qemu-riscv64-virt/make.mk
+++ platforms/qemu-riscv64-virt/make.mk
@@ -12,7 +12,7 @@ instructions:=$(bao_demos)/platforms/$(PLATFORM)/README.md
 run: qemu platform
 	$(call print-instructions, $(instructions), 1, true)
 	$(qemu_cmd) -nographic\
-		-M virt -cpu rv64 -m 4G -smp 4\
+		-M virt -cpu rv64,priv_spec=v1.13.0,sstc=true -m 4G -smp 4\
 		-bios $(opensbi_image)\
 		-device virtio-net-device,netdev=net0\
 		-netdev user,id=net0,net=192.168.42.0/24,hostfwd=tcp:127.0.0.1:5555-:22\
