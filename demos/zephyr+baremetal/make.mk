c944061a6bd70a8ea3b13895194eb3075e084a3d
Daniel Oliveira <drawnpoetry@gmail.com>
2026-02-11 13:40:42 +0000

    feat(s32z270): add nxp s32z270 baremetal and zephyr demos
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- demos/zephyr+baremetal/make.mk
+++ demos/zephyr+baremetal/make.mk
@@ -8,9 +8,13 @@ baremetal_image:=$(wrkdir_demo_imgs)/baremetal.bin
 baremetal_args:=APP_SRC_DIR=$(bao_demos)/demos/zephyr+baremetal/baremetal-app
 
 ifeq ($(ARCH_PROFILE),armv8-r)
+ifneq ($(PLATFORM),s32z270)
 baremetal_args+=MEM_BASE=0x10000000
 fvpr_image_data:=$(baremetal_image)@0x10000000 $(zephyr_image)@0x24000000
 baremetal_args+=SHMEM_BASE=0x70000000 SHMEM_SIZE=0x200000
+else
+baremetal_args+=SHMEM_BASE=0x31840000 SHMEM_SIZE=0x4000
+endif
 else
 baremetal_args+=STD_ADDR_SPACE=y
 baremetal_args+=SHMEM_BASE=0xf0000000 SHMEM_SIZE=0x200000
