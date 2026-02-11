ae94ea8684a1d52dbbb522cecef242219c652cd7
Daniel Oliveira <drawnpoetry@gmail.com>
2026-02-11 14:09:17 +0000

    fix(baremetal): add timer enable and format
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- demos/baremetal/src/demo.c
+++ demos/baremetal/src/demo.c
@@ -1,72 +1,74 @@
-/** 
- *
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) Bao Project and Contributors. All rights reserved.
  */
 
- #include <core.h>
- #include <stdlib.h>
- #include <stdio.h>
- #include <string.h> 
- #include <cpu.h>
- #include <wfi.h>
- #include <spinlock.h>
- #include <plat.h>
- #include <irq.h>
- #include <uart.h>
- #include <timer.h>
- 
- #define TIMER_INTERVAL (TIME_S(1))
- 
- spinlock_t print_lock = SPINLOCK_INITVAL;
- 
- void uart_rx_handler(){
-     static int irq_count = 0;
-     uart_clear_rxirq();
-     printf("cpu%d: %s %d\n",get_cpuid(), __func__, ++irq_count);
- }
- 
- void ipi_handler(){
-     printf("cpu%d: %s\n", get_cpuid(), __func__);
-     irq_send_ipi(1ull << (get_cpuid() + 1));
- }
- 
- void timer_handler(){
-     printf("cpu%d: %s\n", get_cpuid(), __func__);
-     timer_set(TIMER_INTERVAL);
-     irq_send_ipi(1ull << (get_cpuid() + 1));
- }
- 
- void main(void){
- 
-     static volatile bool master_done = false;
+#include <core.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <cpu.h>
+#include <wfi.h>
+#include <spinlock.h>
+#include <plat.h>
+#include <irq.h>
+#include <uart.h>
+#include <timer.h>
 
-     if(cpu_is_master()){
-         spin_lock(&print_lock);
-         printf("Bao bare-metal test guest\n");
-         spin_unlock(&print_lock);
- 
-         irq_set_handler(UART_IRQ_ID, uart_rx_handler);
-         irq_set_handler(TIMER_IRQ_ID, timer_handler);
-         irq_set_handler(IPI_IRQ_ID, ipi_handler);
- 
-         uart_enable_rxirq();
- 
-         timer_set(TIMER_INTERVAL);
-         irq_enable(TIMER_IRQ_ID);
-         irq_set_prio(TIMER_IRQ_ID, IRQ_MAX_PRIO);
- 
-         master_done = true;
-     }
- 
-     irq_enable(UART_IRQ_ID);
-     irq_set_prio(UART_IRQ_ID, IRQ_MAX_PRIO);
-     irq_enable(IPI_IRQ_ID);
-     irq_set_prio(IPI_IRQ_ID, IRQ_MAX_PRIO);
- 
-     while(!master_done);
-     spin_lock(&print_lock);
-     printf("cpu %d up\n", get_cpuid());
-     spin_unlock(&print_lock);
- 
-     while(1) wfi();
- }
- 
+#define TIMER_INTERVAL (TIME_S(1))
+
+spinlock_t print_lock = SPINLOCK_INITVAL;
+
+void uart_rx_handler(){
+    static int irq_count = 0;
+    uart_clear_rxirq();
+    printf("cpu%d: %s %d\n",get_cpuid(), __func__, ++irq_count);
+}
+
+void ipi_handler(){
+    printf("cpu%d: %s\n", get_cpuid(), __func__);
+    irq_send_ipi(1ull << (get_cpuid() + 1));
+}
+
+void timer_handler(){
+    printf("cpu%d: %s\n", get_cpuid(), __func__);
+    timer_set(TIMER_INTERVAL);
+    irq_send_ipi(1ull << (get_cpuid() + 1));
+}
+
+void main(void){
+
+    static volatile bool master_done = false;
+
+    if(cpu_is_master()){
+        spin_lock(&print_lock);
+        printf("Bao bare-metal test guest\n");
+        spin_unlock(&print_lock);
+
+        irq_set_handler(UART_IRQ_ID, uart_rx_handler);
+        irq_set_handler(TIMER_IRQ_ID, timer_handler);
+        irq_set_handler(IPI_IRQ_ID, ipi_handler);
+
+        uart_enable_rxirq();
+
+        timer_set(TIMER_INTERVAL);
+        irq_enable(TIMER_IRQ_ID);
+        irq_set_prio(TIMER_IRQ_ID, IRQ_MAX_PRIO);
+
+        timer_enable();
+
+        master_done = true;
+    }
+
+    irq_enable(UART_IRQ_ID);
+    irq_set_prio(UART_IRQ_ID, IRQ_MAX_PRIO);
+    irq_enable(IPI_IRQ_ID);
+    irq_set_prio(IPI_IRQ_ID, IRQ_MAX_PRIO);
+
+    while(!master_done);
+    spin_lock(&print_lock);
+    printf("cpu %d up\n", get_cpuid());
+    spin_unlock(&print_lock);
+
+    while(1) wfi();
+}
