1e0a54bffda26e004239dc2d550e31f715c74faa
João Peixoto <joaopeixotooficial@gmail.com>
2026-02-14 11:16:15 +0000

    feat(plat): NXP S32G3 support
    Signed-off-by: João Peixoto <joaopeixotooficial@gmail.com>

--- guests/linux/buildroot/external/package/bao-drivers/bao-drivers.mk
+++ guests/linux/buildroot/external/package/bao-drivers/bao-drivers.mk
@@ -1,5 +1,10 @@
 BAO_DRIVERS_SITE = https://github.com/bao-project/bao-linux-drivers.git
+ifneq (,$(filter $(PLATFORM),s32g3))
+BAO_DRIVERS_VERSION = linux-v6.6
+else
 BAO_DRIVERS_VERSION = linux-v6.15
+endif
+
 BAO_DRIVERS_SITE_METHOD = git
 
 BAO_DRIVERS_MODULE_SUBDIRS = ipc iodispatcher
