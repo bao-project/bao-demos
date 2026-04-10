737f4ab39c4185345e22202338ee6df57a2bdadb
leon <uomleon@163.com>
2026-04-10 18:00:46 +0800

    update(make): temporary change baremetal src, will revert once pr is ready to merge

--- guests/baremetal/make.mk
+++ guests/baremetal/make.mk
@@ -1,6 +1,6 @@
 baremetal_src:=$(wrkdir_src)/baremetal
-baremetal_repo:=https://github.com/bao-project/bao-baremetal-guest.git
-baremetal_branch:=demo
+baremetal_repo:=https://github.com/leon6002/bao-baremetal-guest.git
+baremetal_branch:=fix/e3650-console
 
 $(baremetal_src):
 	git clone $(baremetal_repo) $@ --branch $(baremetal_branch)
